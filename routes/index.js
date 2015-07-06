abc08f8486bfc54fcf87c52e33aa20ce0194a5f7
acmedina <a_cabeza_medina@hotmail.com>
2015-07-06 22:23:40 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
@@ -16,4 +19,5 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author', function(req, res){
 	res.render('author');
 });
+
 module.exports = router;
